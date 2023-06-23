e6e91b64303bfd7060beebe92eae9a0774d45604
ahmettsamsunn <113214856+ahmettsamsunn@users.noreply.github.com>
2023-06-23 04:18:11 +0300

    Initial Commit

---
+++ rickandmorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickandmorty
+//
+//  Created by Ahmet Samsun on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
